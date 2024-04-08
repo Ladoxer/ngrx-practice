19a2cd87069ecea24eba2e2e99ddecd04aea8699
Ladoxer <ladderdoxer@gmail.com>
2024-04-08 11:59:36 +0530

    added ngrx and finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 import { CounterOutputComponent } from './counter-output/counter-output.component';
 import { CounterControlsComponent } from './counter-controls/counter-controls.component';
+import { counterReducer } from './store/counter.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { CounterEffects } from './store/counter.effects';
 
 @NgModule({
   declarations: [
@@ -11,7 +15,9 @@ import { CounterControlsComponent } from './counter-controls/counter-controls.co
     CounterOutputComponent,
     CounterControlsComponent,
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, StoreModule.forRoot({
+    counter: counterReducer
+  }), EffectsModule.forRoot([CounterEffects])],
   providers: [],
   bootstrap: [AppComponent],
 })
